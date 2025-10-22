36501d9fdbd9684c151ec51ac33f1c917d2569a5
syedarfeen7 <syedarfeen7890@gmail.com>
2025-10-22 20:01:33 +0300

    Update removed react hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,28 +25,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <header>
-        <nav
-          className="p-4 shadow-md flex items-center justify-between"
-          style={{ backgroundColor: "#f0f0f0" }}
-        >
-          <h1 className="text-2xl font-bold">My App</h1>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link href={"/about"}>About</Link>
-            </li>
-            <li>
-              <Link href={"/"}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          <nav
+            className="p-4 shadow-md flex items-center justify-between"
+            style={{ backgroundColor: "#f0f0f0" }}
+          >
+            <h1 className="text-2xl font-bold">My App</h1>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link href={"/about"}>About</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
