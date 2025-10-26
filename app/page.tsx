b9bad2fc2ebe687d2e4f70d506d15aea74c40bdf
syedarfeen7 <syedarfeen7890@gmail.com>
2025-10-26 19:10:41 +0300

    deleted unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Counter from "./components/Counter";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Welcome to Next.js</h1>
-      <Counter />
     </div>
   );
 }
