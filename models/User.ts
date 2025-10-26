2ed9d460485d60ff4b09d672caaa81ea859c8bf0
syedarfeen7 <syedarfeen7890@gmail.com>
2025-10-26 19:04:49 +0300

    feat(model): User model created with (firstName, lastName, email and image)

---
+++ models/User.ts
@@ -0,0 +1,17 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
