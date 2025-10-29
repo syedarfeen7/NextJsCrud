3a3def8b26387d257f6d41f15706fb2d120d4963
syedarfeen7 <syedarfeen7890@gmail.com>
2025-10-29 17:32:35 +0300

    chore: separate files for types

---
+++ types/user.ts
@@ -0,0 +1,11 @@
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  image: string;
+  createdAt: string;
+}
+
+export type { User };
